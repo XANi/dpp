a4bfdb9f1e6c88227393ed57a7803e65c80c4c0e
XANi <xani666@gmail.com>
2012-11-13 00:51:31 +0100

    add bootstrap file with required packages

---
+++ bootstrap.pp
@@ -0,0 +1,9 @@
+package { [
+           'libev-perl',
+           'libanyevent-perl',
+           'libanyevent-http-perl',
+           'liblog-any-perl',
+           'liblog-any-adapter-dispatch-perl',]:
+               ensure => installed,
+}
+
