034d814e096bfcb26ee71d92af02267698f80916
Mariusz Gronczewski <xani666@gmail.com>
2015-08-31 23:27:17 +0200

    do not install recommends on bootstrap

--- install_deps.sh
+++ install_deps.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 apt-get update
-apt-get install -y git
-apt-get install -y cpanminus
-apt-get install -y puppet
-apt-get install -y make
-apt-get install -y gcc libssl-dev
+apt-get install --no-install-recommends -y git
+apt-get install --no-install-recommends -y cpanminus
+apt-get install --no-install-recommends -y puppet
+apt-get install --no-install-recommends -y make
+apt-get install --no-install-recommends -y gcc libssl-dev
+apt-get install --no-install-recommends -y libyaml-libyaml-perl
 cpanm Carton
 carton install
